e63beca3bfd684525ccb213c905be8b894c9fa8a
Egor <khromove@gmail.com>
2020-04-15 21:54:52 +0700

    add training

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import * as tfvis from '@tensorflow/tfjs-vis'
+import * as tf from '@tensorflow/tfjs'
 
-async function getData() {
+import { RawCar, CleanedCar } from './types'
+
+async function getData(): Promise<CleanedCar[]> {
   const carsDataReq = await fetch('https://storage.googleapis.com/tfjs-tutorials/carsData.json')
-  const carsData = await carsDataReq.json()
+  const carsData: RawCar[] = await carsDataReq.json()
   const cleaned = carsData
     .map((car) => ({
       mpg: car.Miles_per_Gallon,
@@ -13,7 +16,84 @@ async function getData() {
   return cleaned
 }
 
-async function run() {
+/**
+ * Convert the input data to tensors that we can use for machine
+ * learning. We will also do the important best practices of _shuffling_
+ * the data and _normalizing_ the data
+ * MPG on the y-axis.
+ */
+function convertToTensor(data: CleanedCar[]) {
+  // Wrapping these calculations in a tidy will dispose any
+  // intermediate tensors.
+
+  return tf.tidy(() => {
+    // Step 1. Shuffle the data
+    tf.util.shuffle(data)
+
+    // Step 2. Convert data to Tensor
+    const inputs = data.map((d) => d.horsepower)
+    const labels = data.map((d) => d.mpg)
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1])
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1])
+
+    // Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    const inputMax = inputTensor.max()
+    const inputMin = inputTensor.min()
+    const labelMax = labelTensor.max()
+    const labelMin = labelTensor.min()
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin))
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin))
+
+    return {
+      inputs: normalizedInputs,
+      labels: normalizedLabels,
+      // Return the min/max bounds so we can use them later.
+      inputMax,
+      inputMin,
+      labelMax,
+      labelMin,
+    }
+  })
+}
+
+function createModel(): tf.Sequential {
+  // Create a sequential model
+  const model = tf.sequential()
+
+  // Add a single input layer
+  model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }))
+
+  // Add an output layer
+  model.add(tf.layers.dense({ units: 1, useBias: true }))
+
+  return model
+}
+
+async function trainModel(model, inputs, labels) {
+  // Prepare the model for training.
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: tf.losses.meanSquaredError,
+    metrics: ['mse'],
+  })
+
+  const batchSize = 32
+  const epochs = 50
+
+  return model.fit(inputs, labels, {
+    batchSize,
+    epochs,
+    shuffle: true,
+    callbacks: tfvis.show.fitCallbacks({ name: 'Training Performance' }, ['loss', 'mse'], {
+      height: 200,
+      callbacks: ['onEpochEnd'],
+    }),
+  })
+}
+
+async function run(): Promise<void> {
   // Load and plot the original input data that we are going to train on.
   const data = await getData()
   const values = data.map((d) => ({
@@ -32,6 +112,15 @@ async function run() {
   )
 
   // More code will be added below
+  const model = createModel()
+  tfvis.show.modelSummary({ name: 'Model Summary' }, model)
+  // Convert the data to a form we can use for training.
+  const tensorData = convertToTensor(data)
+  const { inputs, labels } = tensorData
+
+  // Train the model
+  await trainModel(model, inputs, labels)
+  console.log('Done Training')
 }
 
 document.addEventListener('DOMContentLoaded', run)
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export type RawCar = {
+  Miles_per_Gallon: number
+  Horsepower: number
+}
+
+export type CleanedCar = {
+  mpg: number
+  horsepower: number
+}
